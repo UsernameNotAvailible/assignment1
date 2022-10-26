bb100794cc956ce7fdb84ad5c3396977d5917713
BuildTools <unconfigured@null.spigotmc.org>
2022-10-26 15:15:40 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // part1?
+        int a; int b; int c; int d; String e; // some variables
+        b = 40; c = 39;  e = "Some words here";
+        System.out.print("a = ");/* No ln to write in the same line */ Scanner a1 = new Scanner (System.in); String a2 = a1.next(); a = Integer.valueOf(a2);
+        System.out.print("d = "); Scanner d1 = new Scanner (System.in); String d2 = d1.next(); d = Integer.valueOf(d2); // one string since it's all basically defining one345 variable
+
+        if ((a >= b && c < d) || (b == d && a != c)) {System.out.println("(a >= b and c < d) or (b == d and a != c)");} // some logic using relational operators
+        else {if (b > a && d <= c) {System.out.println("b > a and d <= c");} // in one string since it's just println, and to be more compact
+        else {System.out.println("Something else");}} // It can be reached
+
+        switch (e) {
+            case "Constant expression required":
+            case "whatever":
+                System.out.println("Some words");
+                break;
+            case "nothing interesting":
+                System.out.println("A word");
+                break;
+            default:
+                System.out.println("No matches");
+                break;
+
+
+        }
     }
 }
--- src/plainConversation.java
+++ src/plainConversation.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class plainConversation {
+import java.util.Scanner;
+public class plainConversation {
+    public static void main(String[] args) {
+        System.out.println("What's your age");
+
+        Scanner userAge = new Scanner(System.in);
+        String userAgeI = userAge.next(); // convert to string
+        double userAgeS = Double.valueOf(userAgeI); // convert to double
+
+        if (userAgeS >= 0 && userAgeS <= 6) {System.out.println("Home, walk and kindergarten.");}
+        else if (userAgeS <= 17 && userAgeS > 6) {System.out.println("You probably go to school");}
+        else if (userAgeS > 17 && userAgeS <= 22) {System.out.println("You probably go to the university");}
+        else if (userAgeS > 22 && userAgeS <= 60) {System.out.println("You probably work");}
+        else if (userAgeS > 60) {System.out.println("You are probably retired");}
+        else {System.out.println("Why did you put a negative number as your age?");}
+
+        if (userAgeS >= 100) {System.out.println("You lived to a hundred, way to go!");}
+
+
+
+
+    }
 }
